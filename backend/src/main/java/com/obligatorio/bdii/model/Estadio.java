2b40c4c88fb024dbf778c93157003cc8338a1f60
manu09810 <manulorenzo19@gmail.com>
2026-06-20 22:10:09 -0300

    Admin ahora puede conseguir los estadio que esta asignado

--- backend/src/main/java/com/obligatorio/bdii/model/Estadio.java
+++ backend/src/main/java/com/obligatorio/bdii/model/Estadio.java
@@ -1,15 +1,16 @@
 package com.obligatorio.bdii.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@Getter @Setter @ToString
+@Getter @Setter @ToString @AllArgsConstructor
 public class Estadio {
 
     private Integer id;
     private String nombre;
     private String ubicacion;
 
-    public Estadio() {}
+  public Estadio (){}
 }
